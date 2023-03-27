47e3ac8422f70616816c30f509b54f83e6d046ad
J2KJonas <75009936+J2KJonas@users.noreply.github.com>
2023-03-27 15:38:11 +0200

    Adding potatoes

--- main_program.py
+++ main_program.py
@@ -1,2 +1,11 @@
 import torch
+import torch.nn as nn
+
+# To save the model, model.state_dict() holds the parameters
+torch.save(model.state_dict(), PATH)
+
+# model must be created again with parameters
+model = Model(*args, **kwargs)
+model.load_state_dict(torch.load(PATH))
+model.eval()
 
