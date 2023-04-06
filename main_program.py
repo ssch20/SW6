8df33db5c95209ab86a2f31eeb80c7d294c1a4fe
J2KJonas <75009936+J2KJonas@users.noreply.github.com>
2023-04-06 21:11:01 +0200

    fixes
    COCO 2017 works but coco14 does not
    Needed:
    - JSON2YOLO, json files need to be converted to labels like in 2017
    - Remove unnecessary code
    - Right now 32 gb of ram is needed to train, need to look into how to change it but try to replace result = model... in main_program.py to the following:
    results = model.train(data="coco.yaml", batch=8. epochs=1)

--- main_program.py
+++ main_program.py
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # device = torch.device('cuda')
     model = YOLO('yolov8n.yaml')  # build a new model from YAML
-    model = YOLO("yolov8x.pt")    # load a pretrained model (recommended for training)
-    model = YOLO('yolov8n.yaml').load('yolov8x.pt')  # build from YAML and transfer weights
+    model = YOLO("yolov8n.pt")    # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
     # Train the model
     results = model.train(data="coco.yaml", epochs=1)  # Train the model
