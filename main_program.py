7911b8caf4b3cce1deebf429afa1995852437794
J2KJonas <75009936+J2KJonas@users.noreply.github.com>
2023-05-03 20:15:37 +0200

    progress
    Error with the fact that the classes are not split up into folders meaning one class in each folder where the images contain the class folders they adhere to.

--- main_program.py
+++ main_program.py
@@ -1,36 +1,108 @@
 """
-Main experiment file. Code adapted from TokenCut: https://github.com/YangtaoWANG95/TokenCut
+Main experiment file
 """
 
 import os
-import argparse
-import random
-import pickle
-import datetime
-import time
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-from PIL import Image
-
 import torch
-import torch.nn as nn
-import numpy as np
+import torch.utils.data
 
+import torchvision.datasets as dset
+
+from torch import nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
 from ultralytics import YOLO
 
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
 
 
+class MLP(nn.Module):
+  '''
+    Multilayer Perceptron.
+  '''
+  def __init__(self):
+    super().__init__()
+    self.layers = nn.Sequential(
+      nn.Flatten(),
+      nn.Linear(32 * 32 * 3, 64),
+      nn.ReLU(),
+      nn.Linear(64, 32),
+      nn.ReLU(),
+      nn.Linear(32, 10)
+    )
 
-if __name__ == "__main__":
 
-    # Model
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    # device = torch.device('cuda')
-    model = YOLO('yolov8n.yaml')  # build a new model from YAML
-    model = YOLO("yolov8n.pt")    # load a pretrained model (recommended for training)
-    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+  def forward(self, x):
+    '''Forward pass'''
+    return self.layers(x)
 
-    # Train the model
-    results = model.train(data="coco.yaml", batch=1, epochs=1)  # Train the model
+
+if __name__ == '__main__':
+  
+  # Model
+  device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+  
+  # Set fixed random number seed
+  torch.manual_seed(42)
+  
+  # Prepare COCO dataset
+  path2data="./datasets/COCO17/images/train2017"
+  path2json="./datasets/COCO17/annotations/instances_train2017.json"
+  coco_train = dset.CocoDetection(root = path2data,
+                                  annFile = path2json)
+  print('Number of samples: ', len(coco_train))
+  #dataset = pathlib.Path('./datasets/COCO14/', transform=transforms.ToTensor())
+  transform = T.ToTensor()
+  dataset = ImageFolder(root=path2data, transform=transform)
+  trainloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=1)
+  
+  # Initialize the MLP
+  mlp = MLP()
+  
+  # Define the loss function and optimizer
+  loss_function = nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+  
+  # Run the training loop
+  for epoch in range(0, 2): # 5 epochs at maximum
+    
+    # Print epoch
+    print(f'Starting epoch {epoch+1}')
+    
+    # Set current loss value
+    current_loss = 0.0
+
+    # Iterate over the DataLoader for training data
+    for i, data in enumerate(trainloader, 0):
+      
+      # Get inputs
+      inputs, targets = data
+      
+      # Zero the gradients
+      optimizer.zero_grad()
+      
+      # Perform forward pass
+      outputs = mlp(inputs)
+      
+      # Compute loss
+      loss = loss_function(outputs, targets)
+      
+      # Perform backward pass
+      loss.backward()
+      
+      # Perform optimization
+      optimizer.step()
+      
+      # Print statistics
+      current_loss += loss.item()
+      if i % 500 == 499:
+          print('Loss after mini-batch %5d: %.3f' %
+                (i + 1, current_loss / 500))
+          current_loss = 0.0
+
+  # Process is complete.
+  print('Training process has finished.')
 
 
